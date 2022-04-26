d23cc9354b92bf7042d4d304d8ba6a334793cb84
Joseph Senecal <jasenecal2@gmail.com>
2022-04-26 14:20:50 -0600

    Fix: correct show path in visit argument

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "merchants discounts show page" do
     discount_1 = merchant_1.bulk_discounts.create!(threshold: 12, discount: 5.0)
     discount_2 = merchant_1.bulk_discounts.create!(threshold: 50, discount: 10.0)
 
-    visit "/merchants/#{merchant_1.id}/discounts/#{discount_1.id}"
+    visit "/merchants/#{merchant_1.id}/bulk_discounts/#{discount_1.id}"
 
     expect(page).to have_content("Threshold: 12 or more items")
     expect(page).to have_content("Discount: 5.0% off item's unit price")
