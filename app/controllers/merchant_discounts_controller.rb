53b5df2852a12aab84abd51bd25415570da3ea10
Joseph Senecal <jasenecal2@gmail.com>
2022-04-26 16:00:33 -0600

    Feat: merchant discount edit

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -23,6 +23,20 @@ class MerchantDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
   end
 
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def update
+    discount = BulkDiscount.find(params[:id])
+    if discount.update(discount_params)
+      redirect_to "/merchants/#{params[:merchant_id]}/bulk_discounts/#{params[:id]}/"
+    else
+      flash[:notice] = "Discount not updated: at least one missing field required"
+      redirect_to "/merchants/#{params[:merchant_id]}/bulk_discounts/#{params[:id]}/edit/"
+    end
+  end
+
   private
 
   def discount_params
