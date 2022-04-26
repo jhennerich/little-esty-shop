208bd7ddba416b63b519c0115de63a2ee3ff1832
Joseph Senecal <jasenecal2@gmail.com>
2022-04-26 09:43:58 -0600

    Test: bulk discount links from merchant dashboard, displays discount info

---
+++ spec/features/merchants/discounts/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "merchants discounts index page" do
+
+  it "links from the merchant's dashboard" do
+    merchant_1 = Merchant.create!(name: "General Paper Inc")
+    discount_1 = merchant_1.BulkDiscount.create!(threshold: 12, discount: 0.05)
+    discount_1 = merchant_1.BulkDiscount.create!(threshold: 50, discount: 0.1)
+
+    visit "/merchants/#{merchant_1.id}/dashboard/"
+    click_on "View My Discounts"
+
+    expect(current_path).to be("/merchants/#{merchant_1.id}/discounts/")
+    expect(page).to have_content("5% when a customer buys 12 or more items")
+    expect(page).to have_content("10% when a customer buys 50 or more items")
+  end
+
+end
