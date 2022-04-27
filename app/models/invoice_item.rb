3a9f114b19f48537f8e0f613bdba7e62ff7af52c
Joseph Senecal <jasenecal2@gmail.com>
2022-04-26 21:30:33 -0600

    Feat: invoice_item model method #get_discount and tests

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -10,4 +10,12 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
 
   enum status: {pending: 0, packaged: 1, shipped: 2}
+
+  def get_discount
+    item = Item.find(item_id)
+    all_discounts = Merchant.find(item.merchant_id).bulk_discounts.order(:discount)
+    discount = nil
+    all_discounts.each { |bulk_discount| discount = bulk_discount if self.quantity >= bulk_discount.threshold }
+    return discount
+  end
 end
